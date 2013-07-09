0ed07b8ac752aa326922354ff3411751f3799161
Jonathan Beebe <me@jonbeebe.net>
2013-07-09 12:38:27 -0700

    Added module snippet, cleaned up new project template.

--- bundle.rb
+++ bundle.rb
@@ -8,98 +8,13 @@ bundle 'Platino SDK' do |bundle|
   bundle.repository = "git://github.com/Lanica/Platino.ruble.git"
 
   bundle.menu t(:bundle_name) do |main_menu|
-    main_menu.command 'Generate Scene Scaffold'
     main_menu.menu 'require() ...' do |require_menu|
       require_menu.command 'Platino'
       require_menu.command 'Box2D (physics)'
     end
-    main_menu.menu 'API Snippets' do |api_menu|
-      api_menu.menu 'GameView' do |m|
-        m.command 'platino.createGameView'
-        m.command 'game.startCurrentScene'
-        m.command 'game.start'
-        m.command 'game.stop'
-        m.command 'game.pushScene'
-        m.command 'game.popScene'
-        m.command 'game.replaceScene'
-        m.command 'game.topScene'
-        m.command 'game.addHUD'
-        m.command 'game.removeHUD'
-        m.command 'game.color'
-        m.command 'game.uptime'
-        m.command 'game.loadTexture'
-        m.command 'game.loadTextureWithTag'
-        m.command 'game.unloadTexture'
-        m.command 'game.unloadTextureByTag'
-        m.command 'game.moveCamera'
-      end
-      api_menu.menu 'Scene' do |m|
-        m.command 'platino.createScene'
-        m.command 'scene.add'
-        m.command 'scene.remove'
-        m.command 'scene.color'
-      end
-      api_menu.menu 'Sprite' do |m|
-        m.command 'platino.createSprite'
-        m.command 'sprite.show'
-        m.command 'sprite.hide'
-        m.command 'sprite.move'
-        m.command 'sprite.rotate'
-        m.command 'sprite.rotateFrom'
-        m.command 'sprite.rotateX'
-        m.command 'sprite.rotateY'
-        m.command 'sprite.rotateZ'
-        m.command 'sprite.rotateFromAxis'
-        m.command 'sprite.scale'
-        m.command 'sprite.scaleBy'
-        m.command 'sprite.scaleFromCenter'
-        m.command 'sprite.color'
-        m.command 'sprite.addTransformChild'
-        m.command 'sprite.addTransformChildWithRelativePosition'
-        m.command 'sprite.removeTransformChild'
-        m.command 'sprite.contains'
-        m.command 'sprite.collidesWith'
-        m.command 'sprite.blendFunc'
-        m.command 'sprite.dispose'
-        m.command 'sprite.transform'
-        m.command 'sprite.clearTransforms'
-        m.command 'sprite.loadTextureByBlob'
-        m.command 'sprite.loadTextureByBlobWithName'
-      end
-      api_menu.menu 'SpriteSheet' do |m|
-        m.command 'platino.createSpriteSheet'
-        m.command 'spritesheet.animate(frames...)'
-        m.command 'spritesheet.animate(start...)'
-        m.command 'spritesheet.stop'
-        m.command 'spritesheet.pause'
-        m.command 'spritesheet.pauseAt'
-      end
-      api_menu.menu 'MapSprite' do |m|
-        m.command 'platino.createMapSprite'
-        m.command 'mapsprite.getTile'
-        m.command 'mapsprite.updateTile'
-        m.command 'mapsprite.removeTile'
-      end
-      api_menu.menu 'TextSprite' do |m|
-        m.command 'platino.createTextSprite'
-        m.command 'textsprite.sizeWithText'
-      end
-      api_menu.menu 'Transform' do |m|
-        m.command 'platino.createTransform'
-        m.command 'transform.move'
-        m.command 'transform.color'
-        m.command 'transform.hide'
-        m.command 'transform.show'
-        m.command 'transform.rotate'
-        m.command 'transform.rotateFrom'
-        m.command 'transform.scale'
-      end
-      api_menu.menu 'Particles' do |m|
-        m.command 'platino.createParticles'
-        m.command 'particle.move'
-        m.command 'particle.dispose'
-      end
-    end
+
+    main_menu.command 'New Scene from Template'
+    main_menu.command 'New Module from Template'
   end
 
   project_template t(:platino_application) do |t|
--- snippets/snippets.rb
+++ snippets/snippets.rb
@@ -1,6 +1,6 @@
 with_defaults :scope => 'meta.project.com.appcelerator.titanium.mobile source.js' do
-  snippet 'Generate Scene Scaffold' do |s|
-    s.trigger = 'gs'
+  snippet 'New Scene from Template' do |s|
+    s.trigger = 'scene'
     s.expansion =
 "var platino = require('co.lanica.platino');
 
@@ -30,177 +30,28 @@ module.exports = ${1:SceneName};
 "
   end
 
-  # ---- BEGIN require() ... ----
+  snippet 'New Module from Template' do |s|
+    s.trigger = 'module'
+    s.expansion = 
+"function ${1:ModuleName}() {
 
-  snippet 'Platino' do |s|
-    s.trigger = 'rp'
-    s.expansion = 'var platino = require(\'co.lanica.platino\');'
+\t// Constructor
+\t${1:ModuleName} = function() {
+
+\t\t${0:TM_SELECTED_TEXT}
+
+\t\treturn this;
+\t};
+
+\t${1:ModuleName}.prototype.sampleMethod = function() {
+\t\tthis.var1 = 'xyz';
+\t};
+
+\treturn ${1:ModuleName};
+};
+
+module.exports = ${1:ModuleName};
+"
   end
-
-  snippet 'Box2D (physics)' do |s|
-    s.trigger = 'rbox2d'
-    s.expansion = 'var box2d = require(\'co.lanica.box2djs\');'
-  end
-
-  snippet 'Chipmunk (physics)' do |s|
-    s.trigger = 'rchipmunk'
-    s.expansion = 'var chipmunk = require(\'co.lanica.chipmunkjs\');'
-  end
-
-  # ---- END require() ... ----
-
-  # ---- BEGIN GameView ----
-  
-  snippet 'platino.createGameView' do |s|
-    s.trigger = 'gv'
-    s.expansion = 'var game = platino.createGameView();'
-  end
-  
-  snippet 'game.startCurrentScene' do |s|
-    s.trigger = 'scs'
-    s.expansion = 'game.startCurrentScene();'
-  end
-
-  snippet 'game.start' do |s|
-    s.trigger = 'gstart'
-    s.expansion = 'game.start();'
-  end
-
-  snippet 'game.stop' do |s|
-    s.trigger = 'gstop'
-    s.expansion = 'game.stop();'
-  end
-
-  snippet 'game.pushScene' do |s|
-    s.trigger = 'gpush'
-    s.expansion = 'game.pushScene(${1:scene});'
-  end
-
-  snippet 'game.popScene' do |s|
-    s.trigger = 'gpop'
-    s.expansion = 'game.popScene();'
-  end
-
-  snippet 'game.replaceScene' do |s|
-    s.trigger = 'greplace'
-    s.expansion = 'game.replaceScene(${1:scene});'
-  end
-
-  snippet 'game.topScene' do |s|
-    s.trigger = 'gtop'
-    s.expansion = 'game.topScene();'
-  end
-
-  snippet 'game.addHUD' do |s|
-    s.trigger = 'gaddhud'
-    s.expansion = 'game.addHUD(${1:sprite});'
-  end
-
-  snippet 'game.removeHUD' do |s|
-    s.trigger = 'gremovehud'
-    s.expansion = 'game.removeHUD(${1:sprite});'
-  end
-
-  snippet 'game.color' do |s|
-    s.trigger = 'gcolor'
-    s.expansion = 'game.color(${1:red}, ${2:green}, ${3:blue});'
-  end
-
-  snippet 'game.uptime' do |s|
-    s.trigger = 'ut'
-    s.expansion = 'game.uptime();'
-  end
-
-  snippet 'game.loadTexture' do |s|
-    s.trigger = 'gt'
-    s.expansion = 'game.loadTexture(${1:textureName});'
-  end
-
-  snippet 'game.loadTextureWithTag' do |s|
-    s.trigger = 'gttag'
-    s.expansion = 'game.loadTextureWithTag(${1:textureName}, ${2:tagName});'
-  end
-
-  snippet 'game.unloadTexture' do |s|
-    s.trigger = 'unloadt'
-    s.expansion = 'game.unloadTexture(${1:textureName});'
-  end
-
-  snippet 'game.unloadTextureByTag' do |s|
-    s.trigger = 'unloadtag'
-    s.expansion = 'game.unloadTextureByTag(${1:tagName});'
-  end
-
-  snippet 'game.stop' do |s|
-    s.trigger = 'gstop'
-    s.expansion = 'game.stop();'
-  end
-
-  snippet 'game.moveCamera' do |s|
-    s.trigger = 'movecam'
-    s.expansion = 'game.moveCamera(${1:transform});'
-  end
-
-  # ---- END GameView ----
-
-  # ---- BEGIN Scene ----
-
-  snippet 'platino.createScene' do |s|
-    s.trigger = 'cs'
-    s.expansion = 'var scene = platino.createScene();'
-  end
-
-  snippet 'scene.add' do |s|
-    s.trigger = 'sadd'
-    s.expansion = 'scene.add(${1:sprite});'
-  end
-
-  snippet 'scene.remove' do |s|
-    s.trigger = 'sremove'
-    s.expansion = 'scene.remove(${1:sprite});'
-  end
-
-  snippet 'scene.color' do |s|
-    s.trigger = 'scolor'
-    s.expansion = 'scene.color(${1:red}, ${2:green}, ${3:blue});'
-  end
-
-  # ---- END Scene ----
-
-  # ---- BEGIN Sprite ----
-
-  # TODO
-
-  # ---- END Sprite ----
-
-  # ---- BEGIN SpriteSheet ----
-
-  # TODO
-
-  # ---- END SpriteSheet ----
-
-  # ---- BEGIN MapSprite ----
-
-  # TODO
-
-  # ---- END MapSprite ----
-
-  # ---- BEGIN TextSprite ----
-
-  # TODO
-
-  # ---- END TextSprite ----
-
-  # ---- BEGIN Transform ----
-
-  # TODO
-
-  # ---- END Transform ----
-
-  # ---- BEGIN Particles ----
-
-  # TODO
-
-  # ---- END Particles ----
   
 end
